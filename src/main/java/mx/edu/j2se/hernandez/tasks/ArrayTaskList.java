82cece2b58870b420da41369a128c0e2fe76ca45
Sebastian Hernandez V <sebastian.hdz.val@gmail.com>
2021-02-28 17:50:51 -0600

    Practice 2 Arrays and Links

---
+++ src/main/java/mx/edu/j2se/hernandez/tasks/ArrayTaskList.java
@@ -0,0 +1,71 @@
+package mx.edu.j2se.hernandez.tasks;
+
+public class ArrayTaskList {
+
+    private Tasks[] arrayTaskList = new Tasks[0];
+
+    public void add (Tasks task) {
+        Tasks newArray[] = new Tasks[arrayTaskList.length + 1];
+        System.arraycopy(arrayTaskList, 0, newArray, 0, arrayTaskList.length);
+
+        newArray[newArray.length - 1] = task;
+
+        arrayTaskList = newArray;
+
+    }
+
+    public boolean remove(Tasks task) {
+        for (int i = 0; i < arrayTaskList.length; i++) {
+            if (task.equals(arrayTaskList[i])) {
+                Tasks newArray[] = new Tasks[arrayTaskList.length - 1];
+                System.arraycopy(arrayTaskList, 0, newArray, 0, i);
+                System.arraycopy(arrayTaskList, i + 1, newArray, i, arrayTaskList.length - i - 1);
+                arrayTaskList = newArray;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int size() {
+        if (getArrayTaskList() == null)
+            return 0;
+        return arrayTaskList.length;
+    }
+
+    public Tasks getTask(int index) {
+        return arrayTaskList[index];
+    }
+
+    public ArrayTaskList incoming(int from, int to) {
+        ArrayTaskList incomingTasksList = new ArrayTaskList();
+        for (int i = 0; i < arrayTaskList.length; i++) {
+            if (arrayTaskList[i].isActive()){
+                if (arrayTaskList[i].isRepetitive()){
+                    int sumTime = arrayTaskList[i].getStartTime();
+                    while (sumTime <= arrayTaskList[i].getEndTime()) {
+                        if (sumTime >= from && sumTime <= to) {
+                            incomingTasksList.add(arrayTaskList[i]);
+                            break;
+                        }
+                        else {
+                            sumTime += arrayTaskList[i].getRepeatInterval();
+                        }
+                    }
+
+                }
+                else {
+                    if (arrayTaskList[i].getTime() >= from && arrayTaskList[i].getTime() <= to){
+                        incomingTasksList.add(arrayTaskList[i]);
+                    }
+                }
+            }
+        }
+
+        return incomingTasksList;
+    }
+
+    public Tasks[] getArrayTaskList() {
+        return arrayTaskList;
+    }
+}
--- src/main/java/mx/edu/j2se/hernandez/tasks/Main.java
+++ src/main/java/mx/edu/j2se/hernandez/tasks/Main.java
@@ -13,8 +13,43 @@ public class Main {
 		// making and inactive task;
 		Tasks inactiveTask = new Tasks("Inactive task", 13);
 
-		System.out.println("Next time of execution for " + repTask.getTitle() + ": " + repTask.nextTimeAfter(55));
+		System.out.println("Next time of execution for " + inactiveTask.getTitle() + ": " + inactiveTask.nextTimeAfter(55));
 
+		// Making lots of active tasks
+		Tasks lunchTask = new Tasks("Lunch time alarm", 12, 252, 24);
+		lunchTask.setActive(true);
+		Tasks tvShowTask = new Tasks("My favorite TV show time", 20, 120, 24);
+		tvShowTask.setActive(true);
+		Tasks meetingTask = new Tasks("Important meeting with customers", 200);
+		meetingTask.setActive(true);
+
+
+		ArrayTaskList taskList = new ArrayTaskList();
+		for (int i = 0; i < taskList.size(); i++) {
+			System.out.println(taskList.getTask(i));
+
+		}
+
+		taskList.add(lunchTask);
+		taskList.add(tvShowTask);
+		taskList.add(meetingTask);
+		taskList.add(nonRepTask);
+
+		System.out.println("Could erase a task form the list: " + taskList.remove(nonRepTask));
+		System.out.println("Size of the list: " + taskList.size());
+		System.out.println("Second task of the list: " + taskList.getTask(1).getTitle());
+
+		for (int i = 0; i < taskList.size(); i++) {
+			System.out.println(taskList.getTask(i).getTitle());
+
+		}
+
+		System.out.println("Incoming tasks from hour 200 to 260");
+		ArrayTaskList incomingTasks = taskList.incoming(200, 260);
+		for (int i = 0; i < incomingTasks.size(); i++) {
+			System.out.println(incomingTasks.getTask(i).getTitle());
+			
+		}
 	
 	}
 	
